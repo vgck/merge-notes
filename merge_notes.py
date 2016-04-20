d04e4d237b3f6e8d0c0fcc50f5ccbe0059535d23
vgck <vgck@list.ru>
2016-04-20 00:04:38 -0400

    Monkey patched necessary functions. Added the merge button an skeleton function to be implemented later.

--- merge_notes.py
+++ merge_notes.py
@@ -4,20 +4,89 @@ from aqt import mw
 from aqt.utils import showInfo
 # import all of the Qt GUI library
 from aqt.qt import *
+# import hooks
+from anki.hooks import addHook
+from anki.hooks import wrap
+# import browser
+from aqt.browser import Browser
 
-# We're going to add a menu item below. First we want to create a function to
-# be called when the menu item is activated.
 
-def testFunction():
-    # get the number of cards in the current collection, which is stored in
-    # the main window
-    cardCount = mw.col.cardCount()
-    # show a message box
-    showInfo("Card count: %d" % cardCount)
+import sre_constants
+import cgi
+import time
+import re
+from operator import  itemgetter
+from anki.lang import ngettext
 
-# create a new menu item, "test"
-action = QAction("test", mw)
-# set it to call testFunction when it's clicked
-mw.connect(action, SIGNAL("triggered()"), testFunction)
-# and add it to the tools menu
-mw.form.menuTools.addAction(action)
+from aqt.qt import *
+import anki
+import aqt.forms
+from anki.utils import fmtTimeSpan, ids2str, stripHTMLMedia, isWin, intTime, isMac
+from aqt.utils import saveGeom, restoreGeom, saveSplitter, restoreSplitter, \
+    saveHeader, restoreHeader, saveState, restoreState, applyStyles, getTag, \
+    showInfo, askUser, tooltip, openHelp, showWarning, shortcut, getBase, mungeQA
+from anki.hooks import runHook, addHook, remHook
+from aqt.webview import AnkiWebView
+from aqt.toolbar import Toolbar
+from anki.consts import *
+from anki.sound import playFromText, clearAudioQueue
+
+def onFindDupes2(self):
+    d = QDialog(self)
+    frm = aqt.forms.finddupes.Ui_Dialog()
+    frm.setupUi(d)
+    restoreGeom(d, "findDupes")
+    fields = sorted(anki.find.fieldNames(self.col, downcase=False))
+    frm.fields.addItems(fields)
+    self._dupesButton = None
+    self._dupesButton2 = None
+    # links
+    frm.webView.page().setLinkDelegationPolicy(
+        QWebPage.DelegateAllLinks)
+    self.connect(frm.webView,
+                 SIGNAL("linkClicked(QUrl)"),
+                 self.dupeLinkClicked)
+    def onFin(code):
+        saveGeom(d, "findDupes")
+    self.connect(d, SIGNAL("finished(int)"), onFin)
+    def onClick():
+        field = fields[frm.fields.currentIndex()]
+        self.duplicatesReport(frm.webView, field, frm.search.text(), frm)
+    search = frm.buttonBox.addButton(
+        _("Search"), QDialogButtonBox.ActionRole)
+    self.connect(search, SIGNAL("clicked()"), onClick)
+    d.show()
+
+def mergeDupes(res):
+    showInfo("To Be Implemented!")
+
+def duplicatesReport2(self, web, fname, search, frm):
+    self.mw.progress.start()
+    res = self.mw.col.findDupes(fname, search)
+    if not self._dupesButton:
+        self._dupesButton = b = frm.buttonBox.addButton(
+            _("Tag Duplicates"), QDialogButtonBox.ActionRole)
+        self.connect(b, SIGNAL("clicked()"), lambda: self._onTagDupes(res))
+    if not self._dupesButton2:
+        self._dupesButton2 = b2 = frm.buttonBox.addButton(
+            _("Merge Duplicates"), QDialogButtonBox.ActionRole)
+        self.connect(b2, SIGNAL("clicked()"), lambda: mergeDupes(res))
+    t = "<html><body>"
+    groups = len(res)
+    notes = sum(len(r[1]) for r in res)
+    part1 = ngettext("%d group", "%d groups", groups) % groups
+    part2 = ngettext("%d note", "%d notes", notes) % notes
+    t += _("Found %(a)s across %(b)s.") % dict(a=part1, b=part2)
+    t += "<p><ol>"
+    for val, nids in res:
+        t += '<li><a href="%s">%s</a>: %s</a>' % (
+            "nid:" + ",".join(str(id) for id in nids),
+            ngettext("%d note", "%d notes", len(nids)) % len(nids),
+            cgi.escape(val))
+    t += "</ol>"
+    t += "</body></html>"
+    web.setHtml(t)
+    self.mw.progress.finish()
+
+Browser.onFindDupes = onFindDupes2
+Browser.duplicatesReport = duplicatesReport2
