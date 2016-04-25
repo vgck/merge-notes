4bd7d92156b2f482c1e9586ab012f8efd1e295cb
vgck <vgck@list.ru>
2016-04-25 11:11:12 -0400

    More cleanup, fixed button spamming, added checkpoint.

--- merge_notes.py
+++ merge_notes.py
@@ -9,56 +9,50 @@ def mergeDupes(res):
     if not res:
         return
 
-    # Loop through each duplicate
+    mw.checkpoint(_("Merge Duplicates"))
+    
+    def update(ncc, nc):
+        mw.col.db.execute("update cards set mod=?, usn=?, type=?, queue=?, due=?, ivl=?, factor=?, reps=?, lapses=?, left=?, odue=0, odid=0 where id = ?",
+            intTime(), mw.col.usn(), nc.type, nc.queue, nc.due, nc.ivl, nc.factor, nc.reps, nc.lapses, nc.left, ncc.id)
+
     for s, nidlist in res:
-        note = mw.col.getNote(nidlist[0])
-
-        # Create new note and copy all model fields from original note
         note_copy = mw.col.newNote()
-        note_copy.fields = note.fields
-
-        # Add the note to the database now to force anki to generate the cards
-        note_copy.flush()
-        mw.col.addNote(note_copy)
-
-        # Set an initial state for the new cards
-        for ncc, nc in zip(note_copy.cards(), note.cards()):
-            mw.col.db.execute("update cards set mod=?, usn=?, type=?, queue=?, due=?, ivl=?, factor=?, reps=?, lapses=?, left=?, odue=0, odid=0 where id = ?",
-                intTime(), mw.col.usn(), nc.type, nc.queue, nc.due, nc.ivl, nc.factor, nc.reps, nc.lapses, nc.left, ncc.id)
-
-        # Loop through each duplicate note
-        for nid in nidlist:
+        for i, nid in enumerate(nidlist):
             n = mw.col.getNote(nid)
             note_copy.tags += n.tags
 
-            # For each field that is unique and not blank, append to the new note
+            # Add note to database now to force anki to generate cards, then copy an initial state for the new cards
+            if (i == 0):
+                note_copy.fields = n.fields
+                mw.col.addNote(note_copy)
+                for ncc, nc in zip(note_copy.cards(), n.cards()):
+                    update(ncc, nc)
+
             for (name, value) in note_copy.items():
                 arr = value.split(" / ")
                 if (n[name] not in arr and n[name] != ""):
                     note_copy[name] = value + " / " + n[name]
 
-            # Clone card scheduling
             for ncc, nc in zip(note_copy.cards(), n.cards()):
                 if nc.ivl > ncc.ivl or nc.queue > ncc.queue:
-                    mw.col.db.execute("update cards set mod=?, usn=?, type=?, queue=?, due=?, ivl=?, factor=?, reps=?, lapses=?, left=?, odue=0, odid=0 where id = ?",
-                        intTime(), mw.col.usn(), nc.type, nc.queue, nc.due, nc.ivl, nc.factor, nc.reps, nc.lapses, nc.left, ncc.id)
+                    update(ncc, nc)
         mw.col.remNotes(nidlist)
-
-        # Refresh note
         note_copy.flush()
 
-    # Reset collection and main window
+    mw.progress.finish()
     mw.col.reset()
     mw.reset()
+    tooltip(_("Notes merged."))
 
-    mw.progress.finish()
-
-    tooltip(_("Notes merged."), period=1000)
+def onFindDupesWrap(self):
+    self._dupesButton2 = None
 
 def duplicatesReportWrap(self, web, fname, search, frm):
     res = self.mw.col.findDupes(fname, search)
-    self._dupesButton2 = b2 = frm.buttonBox.addButton(
-        _("Merge Duplicates"), QDialogButtonBox.ActionRole)
-    self.connect(b2, SIGNAL("clicked()"), lambda: mergeDupes(res))
+    if not self._dupesButton2:
+        self._dupesButton2 = b2 = frm.buttonBox.addButton(
+            _("Merge Duplicates"), QDialogButtonBox.ActionRole)
+        self.connect(b2, SIGNAL("clicked()"), lambda: mergeDupes(res))
 
+Browser.onFindDupes = wrap(Browser.onFindDupes, onFindDupesWrap)
 Browser.duplicatesReport = wrap(Browser.duplicatesReport, duplicatesReportWrap)
